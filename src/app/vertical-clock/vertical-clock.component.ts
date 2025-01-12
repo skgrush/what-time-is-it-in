a385a3aad16ef1c92fce1c4ce35137aae2314c9f
Samuel K. Grush <Samuel@Grush.org>
2025-01-12 12:06:15 -0600

    touch handling works!

--- src/app/vertical-clock/vertical-clock.component.ts
+++ src/app/vertical-clock/vertical-clock.component.ts
@@ -1,9 +1,102 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output, OutputEmitterRef } from '@angular/core';
 import { ITimeHighlight } from '../time-highlight-modal/time-highlight';
 import { ClockHighlightComponent } from './clock-highlight/clock-highlight.component';
 import { hourHeightPx, hoursOffset, maxHour, minHour } from '../time-highlight-modal/time-utils';
 
 const hourTicks = Object.freeze(Array(maxHour - minHour).fill(0).map((_, i) => i + minHour));
+
+class TouchHandler {
+  readonly #pixelsToTriggerScroll = hourHeightPx;
+
+  #state?: {
+    readonly id: number;
+    readonly lastSetPositionY: number,
+  };
+  readonly #scrolled: OutputEmitterRef<{ hours: number }>;
+
+  constructor(scrollOutput: OutputEmitterRef<{ hours: number }>) {
+    this.#scrolled = scrollOutput;
+  }
+
+  handleTouchEvent(e: TouchEvent) {
+    if (e.type === 'touchstart') {
+      this.#start(e.changedTouches[0]);
+      return;
+    }
+
+    const touch = this.#findTouch(e.changedTouches);
+    if (!touch) {
+      return;
+    }
+
+    switch (e.type) {
+      case 'touchcancel': return this.#cancel(touch);
+      case 'touchend': return this.#end(touch);
+      case 'touchmove': return this.#move(touch);
+    }
+  }
+
+  #findTouch(touchList: TouchList) {
+    const id = this.#state?.id;
+    if (id === undefined) {
+      return undefined;
+    }
+    for (let i = 0; i < touchList.length; ++i) {
+      const touch = touchList[i];
+      if (touch.identifier === id) {
+        return touch;
+      }
+    }
+    return undefined;
+  }
+
+  #start(touch: Touch) {
+    if (this.#state !== undefined) {
+      // already looking at a different touchId
+      return;
+    }
+
+    this.#state = {
+      id: touch.identifier,
+      lastSetPositionY: touch.clientY,
+    };
+  }
+
+  #cancel(touch: Touch) {
+    this.#state = undefined;
+  }
+
+  #end(touch: Touch) {
+    this.#handleTouchChange(touch);
+    this.#state = undefined;
+  }
+
+  #move(touch: Touch) {
+    this.#handleTouchChange(touch);
+  }
+
+  #handleTouchChange(touch: Touch) {
+    const state = this.#state!;
+
+    const { clientY } = touch;
+    const deltaPx = clientY - state.lastSetPositionY;
+
+    if (Math.abs(deltaPx) < this.#pixelsToTriggerScroll) {
+      console.info('touch NO change', touch.identifier, deltaPx);
+      return;
+    }
+    console.info('touch change', touch.identifier, deltaPx);
+
+    const scrollCount = (deltaPx / this.#pixelsToTriggerScroll) | 0;
+    const newPixelPosition = state.lastSetPositionY + (scrollCount * this.#pixelsToTriggerScroll);
+
+    this.#state = {
+      ...state,
+      lastSetPositionY: newPixelPosition,
+    };
+    this.#scrolled.emit({ hours: scrollCount });
+  }
+}
 
 @Component({
   selector: 'wtiii-vertical-clock',
@@ -24,6 +117,10 @@ const hourTicks = Object.freeze(Array(maxHour - minHour).fill(0).map((_, i) => i
     '(keydown.arrowDown)': 'scrolled.emit({ hours: 1 })',
     '(keydown.arrowUp)': 'scrolled.emit({ hours: -1 })',
     '(wheel)': 'wheel($event)',
+    '(touchstart)': 'touchHandler.handleTouchEvent($event)',
+    '(touchcancel)': 'touchHandler.handleTouchEvent($event)',
+    '(touchend)': 'touchHandler.handleTouchEvent($event)',
+    '(touchmove)': 'touchHandler.handleTouchEvent($event)',
   }
 })
 export class VerticalClockComponent {
@@ -38,7 +135,9 @@ export class VerticalClockComponent {
 
   public readonly scrolled = output<{ hours: number }>();
 
-  readonly #pixelsToTriggerScroll = 8;
+  protected readonly touchHandler = new TouchHandler(this.scrolled);
+
+  readonly #pixelsToTriggerScroll = hourHeightPx;
   #scrollPxs = 0;
 
   protected wheel(e: WheelEvent) {
